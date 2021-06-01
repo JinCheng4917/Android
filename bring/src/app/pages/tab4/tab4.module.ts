423de95df685f66c43f2253b6919b06212463c5e
timer <16622504835@163.com>
2021-06-01 21:17:18 +0800

    发布消息列表界面

--- bring/src/app/pages/tab4/tab4.module.ts
+++ bring/src/app/pages/tab4/tab4.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 import { Tab4Page } from './tab4.page';
+import {MessagePageModule} from "../message/message.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab4PageRoutingModule
+    Tab4PageRoutingModule,
+      MessagePageModule
   ],
   declarations: [Tab4Page]
 })
