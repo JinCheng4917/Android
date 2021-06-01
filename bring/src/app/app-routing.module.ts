507304950131db02aac0062b7f656b782729d6fb
jincheng4917 <3300491785@qq.com>
2021-06-01 21:30:44 +0800

    个人中心界面

--- bring/src/app/app-routing.module.ts
+++ bring/src/app/app-routing.module.ts
@@ -1,28 +1,34 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {NgxEchartsModule} from "ngx-echarts";
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'orderDetail/:id',
-    loadChildren: () => import('./pages/order-detail/order-detail.module').then( m => m.OrderDetailPageModule)
-  },
-  {
-    path: 'tab4',
-    loadChildren: () => import('./pages/tab4/tab4.module').then( m => m.Tab4PageModule)
-  },
-  {
-    path: 'tab5',
-    loadChildren: () => import('./pages/tab5/tab5.module').then( m => m.Tab5PageModule)
-  }
+    {
+        path: '',
+        loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    },
+    {
+        path: 'orderDetail/:id',
+        loadChildren: () => import('./pages/order-detail/order-detail.module').then(m => m.OrderDetailPageModule)
+    },
+    {
+        path: 'tab4',
+        loadChildren: () => import('./pages/tab4/tab4.module').then(m => m.Tab4PageModule)
+    },
+    {
+        path: 'tab5',
+        loadChildren: () => import('./pages/tab5/tab5.module').then(m => m.Tab5PageModule)
+    }
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
+        NgxEchartsModule.forRoot({
+            echarts: () => import('echarts')
+        })
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- bring/src/app/pages/tab5/tab5.page.ts
+++ bring/src/app/pages/tab5/tab5.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemeOption} from "ngx-echarts";
+import {CoolTheme} from "./CoolTheme";
 
 @Component({
   selector: 'app-tab5',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
+  initOpts = {
+    renderer: 'svg',
+    width: 350,
+    height: 350
+  };
+
+  options = {
+    color: ['#3398DB'],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    yAxis: [{
+      type: 'value'
+    }],
+    series: [{
+      name: 'Counters',
+      type: 'bar',
+      barWidth: '40%',
+      data: [10, 52, 200, 334, 390, 330, 220]
+    }]
+  };
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
